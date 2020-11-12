672385564248f655fad905f46a0d0dc8ed116390
aleksandarmlinar <60160624+aleksandarmlinar@users.noreply.github.com>
2020-11-12 16:52:39 +0100

    12/11

---
+++ Forelasning/F2/slutkod/script/game.js
@@ -0,0 +1,82 @@
+/*
+    <img src="https://openclipart.org/download/282132/Die6.svg" />
+    <img src="https://openclipart.org/download/282131/Die5.svg" />
+    <img src="https://openclipart.org/download/282130/Die4.svg" />
+    <img src="https://openclipart.org/download/282129/Die3.svg" />
+    <img src="https://openclipart.org/download/282128/Die2.svg" />
+    <img src="https://openclipart.org/download/282127/Die1.svg" />              
+*/
+
+'use strict';
+
+//addEventListener('load', function() {
+
+$( document ).ready( function() {
+
+    console.log('ready');
+
+    $( document ).on('keydown', function(e) {
+
+        console.log('keydown', e.key);
+
+        if(e.key === 'd') {
+
+            console.log('d');
+
+            let rndValue = 0;
+            //let mainRef = $('main');
+
+            const PATH = 'https://openclipart.org/download/2821';
+            let imgRef = null;
+
+            //Tillägg tabort alla bilder som finns i main innan nya tärningar slumpas!
+            //-->
+            /*
+            let imgRefs = mainRef.querySelectorAll('img');
+            for(let j=0; j < imgRefs.length; j++) {
+                imgRefs.item(j).remove();
+            }
+            */
+            
+            $('body main').empty();
+
+            //<--
+
+            //Summan av alla tärning för ett kast...
+            let sum = 0;
+
+            for(let i = 1; i <= 6; i++) {
+
+                rndValue = Math.floor( Math.random() * 6) + 1;
+
+                //Summan
+                sum += rndValue;
+
+                //imgRef = document.createElement('img');
+                imgRef = $('<img>');
+
+                //imgRef.setAttribute('alt', 'Dice');
+                imgRef.attr('alt', 'Dice');
+
+                console.log(rndValue);
+
+                //imgRef.setAttribute('src', PATH + (rndValue + 26) + '/Die' + rndValue + '.svg');
+                imgRef.attr('src', PATH + (rndValue + 26) + '/Die' + rndValue + '.svg');
+
+                //imgRef.style.width = '10%';
+                //imgRef.style.height = '10%';
+                imgRef.css({ 'width' : '10%', 'height' : '10%' });
+
+                //mainRef.appendChild(imgRef);
+                imgRef.appendTo('body main');
+
+            }
+
+            //Summan
+            console.log('Summan är', sum);
+
+        }
+
+    });
+
+});
